305c51d907ad3cde6a9fbfa17c3dab8052d15c0e
Arm <bruniusa@gmail.com>
2022-09-30 15:36:32 +0100

    JS Changes: 1

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,4 @@
 
-const myImage = document.querySelector('img');
-
-myImage.onclick = () => {
-  const mySrc = myImage.getAttribute('src');
-  if (mySrc ==='img/Capy.webp') {
-    myImage.setAttribute('src','img/pluto_cover_EXTRA_EDITED_THICC_BOI.jpg');
-  } else {
-    myImage.setAttribute('src','img/Capy.webp');
-  }
-}
 var modal = document.getElementById("myModal");
 var span = document.getElementsByClassName("close")[0];
 
@@ -25,17 +15,9 @@ enterButton.onclick = ()=>{
     localStorage.setItem('name', myName);
     myHeading.textContent = `Ok I pull up to, ${myName}`;
 }
-// span.onclick = ()=>{
-//     modal.style.display ="none"
-// }
 myButton.onclick = () => {
  signIN();
 }
-// window.onclick = function(event){
-//     if (event.target == modal){
-//         modal.style.display = "none"
-//     }
-// }
 if (!localStorage.getItem('name')) {
     setUserName();
     } else {
